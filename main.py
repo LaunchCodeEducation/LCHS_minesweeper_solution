519dfa8b2ca23a136236d2df0d99f4a48092794d
jimflores <jflores@launchcode.org>
2021-05-13 14:19:44 -0500

    Added column and row labels to session.

--- main.py
+++ main.py
@@ -8,22 +8,21 @@ app.secret_key = 'K>~EEAnH_x,Z{q.43;NmyQiNz1^Yr7'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        session['num_mines'] = int(request.form['num_mines'])
-        if session['num_mines'] == 0:
+        num_mines = request.form['num_mines']
+        if not num_mines or num_mines.isalpha():
             session['num_mines'] = 10
-        reset_board()
+        else:
+            session['num_mines'] = int(num_mines)
         session['mines'] = place_mines(session['num_mines'])
         return redirect('/play')
     else:
-        reset_board()
         session['flags'] = []
         session['guesses'] = []
         session['mine_counts'] = {}
         session['hit_mine'] = False
-    columns = make_columns()
-    rows = make_rows()
-    return render_template("index.html", columns = columns, rows = rows,
-        page_title = "Play Minesweeper")
+    session['columns'] = make_columns()
+    session['rows'] = make_rows()
+    return render_template("index.html", page_title = "Play Minesweeper")
 
 @app.route('/play', methods=['GET', 'POST'])
 def play():
@@ -36,11 +35,8 @@ def play():
         if not safe_guess:
             session['hit_mine'] = True
 
-    columns = make_columns()
-    rows = make_rows()
     page_title = f"Mines Remaining: {session['num_mines']}"
-    return render_template("mines.html", columns = columns, rows = rows,
-        page_title = page_title)
+    return render_template("mines.html", page_title = page_title)
 
 if __name__ == '__main__':
     app.run()
