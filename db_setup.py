b0f770c08b2b69b80f213a27a64f8ea7f6017193
Jim Flores <jimflores5@gmail.com>
2021-05-13 01:17:12 -0500

    Added clickable tables and AI ship placement.

--- db_setup.py
+++ db_setup.py
@@ -1,38 +1,39 @@
 import sqlite3
 import string
 
-ships = ['Destroyer', 'Submarnine', 'Crusier', 'Battleship', 'Carrier']
+ships = [['Destroyer', 2], ['Submarnine', 3], ['Crusier', 3], ['Battleship', 4], ['Carrier', 5]]
 rows = string.ascii_uppercase[0:10]
 columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 db = sqlite3.connect('game.db')
 cursor = db.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS users")
-cursor.execute("DROP TABLE IF EXISTS posts")
+cursor.execute("DROP TABLE IF EXISTS ships")
+cursor.execute("DROP TABLE IF EXISTS cells")
 
 sql_query = """
-    CREATE TABLE IF NOT EXISTS users 
-    (user_id INTEGER PRIMARY KEY, username TEXT NOT NULL)
+    CREATE TABLE IF NOT EXISTS ships 
+    (ship_id INTEGER PRIMARY KEY, name TEXT NOT NULL, length INT NOT NULL)
     """
 cursor.execute(sql_query)
 
 sql_query = """
-    CREATE TABLE IF NOT EXISTS posts 
-    (post_id INTEGER PRIMARY KEY, user_id INT NOT NULL, entry TEXT, time TEXT,
-    FOREIGN KEY (user_id)
-       REFERENCES users (user_id))
+    CREATE TABLE IF NOT EXISTS cells 
+    (cell_id INTEGER PRIMARY KEY, coordinates TEXT NOT NULL, ship_id INT, empty BOOLEAN, guessed TEXT,
+    FOREIGN KEY (ship_id)
+       REFERENCES ships (ship_id))
     """
 cursor.execute(sql_query)
 
-for user in users:
-    sql_query = f"INSERT INTO users (username) VALUES ('{user}')"
+for ship in ships:
+    sql_query = f"INSERT INTO ships (name, length) VALUES ('{ship[0]}', {ship[1]})"
     cursor.execute(sql_query)
 
-for post in posts:
-    rand_id = random.randint(1, len(users))
-    sql_query = f"INSERT INTO posts (user_id, entry, time) VALUES ({rand_id}, '{post}', '{datetime.datetime.now()}')"
-    cursor.execute(sql_query)
+for row in rows:
+    for column in columns:
+        location = row + str(column)
+        sql_query = f"INSERT INTO cells (coordinates, empty) VALUES ('{location}', True)"
+        cursor.execute(sql_query)
 
 db.commit()
 db.close()
--- main.py
+++ main.py
@@ -7,6 +7,28 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.secret_key = 'K>~EEAnH_x,Z{q.43;NmyQiNz1^Yr7'
 
+def reset_board():
+    sql_query = f"UPDATE cells SET ship_id = NULL, empty = True"
+    execute_query(sql_query)
+
+def execute_query(query_string):
+    db = sqlite3.connect('game.db')
+    cursor = db.cursor()
+    if "select" in query_string.lower():
+        try:
+            results = list(cursor.execute(query_string))
+        except:
+            results = 'error'
+    else:
+        try:
+            cursor.execute(query_string)
+            db.commit()
+            results = 'success'
+        except:
+            results = 'error'
+    db.close()
+    return results
+
 def make_columns():
     numbers = ['']
     for index in range(10):
@@ -22,34 +44,136 @@ def make_rows():
             if entry == 0:
                 row.append(letter)
             else:
-                row.append([letter + str(entry), 'empty'])
+                row.append(letter + str(entry))
         rows.append(row)
     return rows.copy()
 
-def place_ships(ship_length = 5):
-    orientation = random.randint(0, 1)
+def place_ship(ship_id, ship_length):
+    valid_placement = False
+    while not valid_placement:
+        orientation = random.randint(0, 1)
+        if orientation: # Horizontal orientation
+            row = random.choice(string.ascii_uppercase[0:10])
+            start_column = random.randint(1, 11-ship_length)
+            cells = []
+            for index in range(ship_length):
+                column = start_column + index
+                cells.append(row + str(column))
+        else:
+            column = random.randint(1, 10)
+            start_row = random.randint(0, 10-ship_length)
+            cells = []
+            for index in range(ship_length):
+                row = string.ascii_uppercase[start_row + index]
+                cells.append(row + str(column))
+        valid_placement = record_postion(cells, ship_id)
+    return cells.copy()
+
+def record_postion(coords, ship):
+    for cell in coords:
+        sql_query = f"SELECT ship_id FROM cells WHERE coordinates = '{cell}' AND empty = False"
+        check_coord = execute_query(sql_query)
+        if check_coord:
+            return False
+    for cell in coords:
+        sql_query = f"UPDATE cells SET ship_id = {ship}, empty = False WHERE coordinates = '{cell}'"
+        execute_query(sql_query)
+    return True
+
+def check_guess(guess):
+    sql_query = f"SELECT ship_id FROM cells WHERE coordinates = '{guess}' AND empty = False"
+    is_hit = execute_query(sql_query)
+    if is_hit:
+        sql_query = f"UPDATE cells SET guessed = 'Hit' WHERE coordinates = '{guess}'"
+        session['hits'].append(guess)
+    else:
+        sql_query = f"UPDATE cells SET guessed = 'Miss' WHERE coordinates = '{guess}'"
+        session['misses'].append(guess)
+    session.modified = True
+    execute_query(sql_query)
+
+def player_ship(ship_name, orientation, start_cell):
+    start_row = start_cell[0]
+    start_column = int(start_cell[1:])
+    cells = []
+    fleet = ['Destroyer', 'Submarine', 'Crusier', 'Battleship', 'Carrier']
+    if ship_name == 'Destroyer':
+        ship_length = 2
+    elif ship_name == 'Submarine' or ship_name == 'Crusier':
+        ship_length = 3
+    elif ship_name == 'Battleship':
+        ship_length = 4
+    else:
+        ship_length = 5
     if orientation: # Horizontal orientation
-        row = random.choice(string.ascii_uppercase[0:10])
-        start_column = random.randint(1, 11-ship_length)
-        cells = []
+        if start_column + ship_length - 1 > 10:
+            return False
         for index in range(ship_length):
             column = start_column + index
-            cells.append(row + str(column))
+            location = start_row + str(column)
+            if location not in session['p_ships']:
+                cells.append(location)
+            else:
+                return False
     else:
-        column = random.randint(1, 10)
-        start_row = random.randint(0, 10-ship_length)
-        cells = []
+        col_index = string.ascii_uppercase.find(start_row)
+        if col_index + ship_length -1 > string.ascii_uppercase.find('J'):
+            return False
         for index in range(ship_length):
-            row = string.ascii_uppercase[start_row + index]
-            cells.append(row + str(column))
+            start = string.ascii_uppercase.find(start_row)
+            row = string.ascii_uppercase[start + index]
+            location = row + str(start_column)
+            if location not in session['p_ships']:
+                cells.append(location)
+            else:
+                return False
+        # valid_placement = record_postion(cells, fleet.index(ship_name)+1)
+    session['p_fleet'].append(ship_name)
+    session.modified = True
     return cells.copy()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        guess = request.form['guess']
+        check_guess(guess)
+    else:
+        reset_board()
+        place_ship(1, 2)
+        place_ship(2, 3)
+        place_ship(3, 3)
+        place_ship(4, 4)
+        place_ship(5, 5)
+        session['hits'] = []
+        session['misses'] = []
     columns = make_columns()
     rows = make_rows()
-    session['cells'] = place_ships()
     return render_template("index.html", columns = columns, rows = rows)
+
+@app.route('/player_board', methods=['GET', 'POST'])
+def player_board():
+    if request.method == 'POST':
+        new_ship = request.form['new_ship']
+        orientation = int(request.form['orientation'])
+        start_cell = request.form['start_cell'].upper()
+        result = player_ship(new_ship, orientation, start_cell)
+        if result: 
+            for cell in result:
+                session['p_ships'].append(cell)
+            session.modified = True
+            feedback = 'Ship placed!'
+        else:
+           feedback = "Invalid placement!"
+        print(session['p_fleet'])
+    else:
+        session['p_ships'] = []
+        session['p_fleet'] = []
+        feedback = ''
+    ships = ['Destroyer','Submarine', 'Crusier','Battleship', 'Carrier']
+    columns = make_columns()
+    rows = make_rows()
+    return render_template("player_board.html", columns = columns, rows = rows, ships=ships,
+        feedback = feedback)
 
 if __name__ == '__main__':
     app.run()
