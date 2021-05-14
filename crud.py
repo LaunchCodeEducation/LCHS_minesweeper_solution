e57662740f2be5a63db0ab9aefdbc56f7b4f3ab1
Jim Flores <jimflores5@gmail.com>
2021-05-14 11:35:46 -0500

    Starter code updates.

--- crud.py
+++ crud.py
@@ -20,34 +20,10 @@ def execute_query(query_string):
     return results
 
 def record_mines(coords):
-    counter = 1
-    for cell in coords:
-        sql_query = f"INSERT INTO mines (coordinates) VALUES ('{cell}')"
-        execute_query(sql_query)
-        sql_query = f"UPDATE map SET mine_id = {counter} WHERE coordinates = '{cell}'"
-        execute_query(sql_query)
-        counter += 1
+    pass
 
 def count_mines():
-    sql_query = "SELECT coordinates FROM map"
-    cells = execute_query(sql_query)
-    for cell in cells:
-        check_surroundings(cell[0])
+    pass
 
 def check_surroundings(cell):
-    count = 0
-    rows = 'XABCDEFGHIJY'
-    row = rows.find(cell[0])
-    column = int(cell[1:])
-    for check_row in rows[row-1:row+2]:
-        for col_change in range(-1, 2):
-            check_column = column + col_change
-            location = check_row + str(check_column)
-            sql_query = f"SELECT mine_id FROM mines WHERE coordinates = '{location}'"
-            mined = execute_query(sql_query)
-            if mined and location != cell:
-                count += 1
-    session['mine_counts'][cell] = count
-    session.modified = True
-    sql_query = f"UPDATE map SET surr_mines = {count} WHERE coordinates = '{cell}'"
-    execute_query(sql_query)
+    pass
