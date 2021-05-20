68f95b1df809b2e246758e3c2788eba281e34847
Jim Flores <jimflores5@gmail.com>
2021-05-20 16:14:29 -0500

    End of index() tutorial.

--- main.py
+++ main.py
@@ -7,13 +7,23 @@ app.config['DEBUG'] = True
 app.secret_key = 'K>~EEAnH_x,Z{q.43;NmyQiNz1^Yr7'
 
 @app.route('/', methods=['GET', 'POST'])
-def index():     
-    reset_board()
+def index():
+    if request.method == "POST":
+        num_mines = request.form['num_mines']
+        if num_mines == '' or not num_mines.isdigit():
+            session['num_mines'] = 10
+        else:
+            session['num_mines'] = int(num_mines)
+        session['mines'] = place_mines(session['num_mines'])
+        return redirect('/play')
+    else:    
+        reset_board()
     return render_template("index.html", page_title = "Play Minesweeper")
 
 @app.route('/play', methods=['GET', 'POST'])
 def play():
-    return render_template("mines.html", page_title = "Mines Remaining: ???")
+    page_title = f"Mines Remaining: {session['num_mines']}"
+    return render_template("mines.html", page_title = page_title)
 
 if __name__ == '__main__':
     app.run()
