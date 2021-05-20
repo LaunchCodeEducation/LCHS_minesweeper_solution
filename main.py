1884dd433bfc8bdac4243fde04ea277c5128c860
Jim Flores <jimflores5@gmail.com>
2021-05-20 11:27:28 -0500

    Starting point for home-logic tutorial.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config['DEBUG'] = True
 app.secret_key = 'K>~EEAnH_x,Z{q.43;NmyQiNz1^Yr7'
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def index():     
     reset_board()
     return render_template("index.html", page_title = "Play Minesweeper")
 
